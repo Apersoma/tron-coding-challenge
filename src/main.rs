39ffec6577c7045bb7b4567729da2d3edb565cb0
Sammy <sammypet3@gmail.com>
2026-04-07 00:30:15 -0400

    fix stuff?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::engine::prelude::*;
+use crate::{engine::prelude::*, players::stardustz_bots::StardustzBot};
 
 mod engine;
 mod players;
@@ -7,9 +7,11 @@ use players::lunar::{Lunar, Rngesus};
 fn main() {
     use players::example_bot::ExampleBot;
     // use players::bot_template::BotTemplate;
+    use players::stardustz_bots::ChaseBot;
+    use players::stardustz_bots::SimpleSpaceFillBot;
     use players::human_controlled_bot::HumanControlledBot;
-    GameEngine::<ExampleBot, Rngesus<3>>::new().run_game();
-    // GameEngine::<ExampleBot, HumanControlledBot>::new().run_game();
-    GameEngine::<HumanControlledBot, ExampleBot>::new().run_game();
-    // GameEngine::<ExampleBot, Rngesus>::new().run_game();
+
+    let mut game: GameEngine<HumanControlledBot, StardustzBot> = GameEngine::new();
+
+    game.run_game();
 }
